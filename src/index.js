bb608bd78e86b87502d5882678feb18e52b70542
Euan_G <euan@amwic.co.za>
2017-07-24 08:55:09 +0200

    Create StorePicker Component

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-// let's go!
+import React from 'react';
+import { render } from 'react-dom';
+import StorePicker from "./components/StorePicker";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <StorePicker />
+      </div>
+    );
+  }
+}
+
+render(<App />, document.querySelector('#main'));
