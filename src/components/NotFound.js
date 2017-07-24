b07aa62e24c178cc08e87eb9cf182d537f51d25a
Euan_G <euan@amwic.co.za>
2017-07-24 10:55:27 +0200

    Add React Router 4

---
+++ src/components/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <div>
+      <H2>Not Found!!!!</H2>
+      I find your lack of navigation disturbing.
+    </div>
+  );
+};
+
+export default NotFound;
