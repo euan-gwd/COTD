ded53d9701e9bd58ba76e681542e7975035cfbc6
Euan_G <euan@amwic.co.za>
2017-07-25 12:40:10 +0200

    Add rebase and bind state to firebase

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyArrqCiyEdsNLzahuKm6wSzrvSzmG4uHbY",
+  authDomain: "cotd-ed1aa.firebaseapp.com",
+  databaseURL: "https://cotd-ed1aa.firebaseio.com",
+});
+
+export default base;
