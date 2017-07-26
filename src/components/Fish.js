c78ecd2580b9fc08db4b53c4dcb686217e5fd819
Euan_G <euan@amwic.co.za>
2017-07-26 11:13:10 +0200

    fix incorrect proptype on fish

--- src/components/Fish.js
+++ src/components/Fish.js
@@ -21,7 +21,7 @@ class Fish extends React.Component {
 
 Fish.propTypes = {
   details: React.PropTypes.object.isRequired,
-  index: React.PropTypes.number.isRequired,
+  index: React.PropTypes.string.isRequired,
   addToOrder: React.PropTypes.func.isRequired
 }
 
